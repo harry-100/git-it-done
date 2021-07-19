5be5b00ffa562e8ffe4353ddfecf2549bd6a38a5
Harry Shah <harvinder.shah@gmail.com>
2021-07-19 16:46:46 -0400

    fetch request made

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,14 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    
+    //format the github api url
+    var apiurl = "https://api.github.com/users/" + user + "/repos";
+    
+    // make a request to the url
 
-getUserRepos();
+    fetch(apiurl).then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    } )
+});
+};
+getUserRepos("microsoft");
